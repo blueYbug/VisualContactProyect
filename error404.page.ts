a4f6178d0c9bda39ff433e5cd9e454333763d6ef
blueYbug <135141724+blueYbug@users.noreply.github.com>
2025-11-13 09:51:42 -0300

    404
    Pagina de redireccion en caso de fallo en la navegación o url.

---
+++ error404.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error404',
+  templateUrl: './error404.page.html',
+  styleUrls: ['./error404.page.scss'],
+  standalone: false
+})
+export class Error404Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
